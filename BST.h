cf2192108de0375e53a143f6da77d29bbf5eedde
Royas Shakya <shkroyas@gmail.com>
2024-05-22 09:48:57 +0545

    Made an Interface class

---
+++ BST.h
@@ -0,0 +1,9 @@
+
+class BST{
+    public:
+        virtual bool isEmpty() = 0;
+        virtual void addBST(int key) = 0;
+        virtual void removeBST(int keyToDelete) = 0;
+        virtual bool searchBST(int targetkey) = 0;
+
+};
