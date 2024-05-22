bcf84235f02f44d925f42cc0464c1a83b94a4461
Royas Shakya <shkroyas@gmail.com>
2024-05-22 10:59:24 +0545

    Main function with implementation

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Array.h"
+
+using namespace std;
+
+int main() {
+    BinarySearchTree bst(10);
+    cout << "Is tree empty? " << bst.isEmpty() << endl;
+    // Testing the binary search tree
+    bst.addBST(50);
+    bst.addBST(30);
+    bst.addBST(70);
+    bst.addBST(20);
+    bst.addBST(40);
+    bst.addBST(60);
+    bst.addBST(80);
+    cout << "Is tree empty? " << bst.isEmpty() << endl;
+    }
