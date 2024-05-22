d6b66f4fd2d41c9f2b08caf9b3787eb794ec3233
Royas Shakya <shkroyas@gmail.com>
2024-05-22 10:18:25 +0545

    Updated BST.h with constructor and destructor

--- BST.h
+++ BST.h
@@ -1,6 +1,11 @@
 
 class BST{
     public:
+        //Constructor
+        BST();
+        //Destructor
+        ~BST();
+        //all the virtual functions for teh Binary Tree
         virtual bool isEmpty() = 0;
         virtual void addBST(int key) = 0;
         virtual void removeBST(int keyToDelete) = 0;
