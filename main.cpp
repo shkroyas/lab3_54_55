070f4ca4bd4d9173dfd69349088d4cc1341b5973
Royas Shakya <shkroyas@gmail.com>
2024-06-05 05:57:21 +0545

    Tested searchBST(key) function for BST Array

--- main.cpp
+++ main.cpp
@@ -18,4 +18,7 @@ int main() {
     cout << "The BST in inorder traversal is: ";
     bst.inorderTraversal(0);
     cout << endl;
+    bst.searchBST(30) ? cout << "Key found" << endl : cout << "Key not found" << endl;
+    bst.searchBST(100)? cout << "Key found" << endl : cout << "Key not found" << endl;
+    
     }
